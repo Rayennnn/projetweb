e1a36e88520b86d4babf6e2c2dca4e7f3de6f058
Rayennnn <rayenbenaissia2004@gmail.com>
2024-11-19 19:37:37 +0100

    projet

---
+++ Controller/bourses.php
@@ -0,0 +1,50 @@
+<?php
+echo'hello';
+/*class Bourse
+{
+    private ?int $idBourse = null;
+    private ?string $nom = null;
+    private ?string $description = null;
+    private ?string $categorie = null;
+    private ?string $pays = null;
+    private ?string $fournisseur = null;
+    private ?string $lien = null;
+    private ?string $dateLimite = null;
+
+    public function __construct($id = null, $nom = null, $desc = null, $cat = null, $pays = null, $fournisseur = null, $lien = null, $date = null)
+    {
+        $this->idBourse = $id;
+        $this->nom = $nom;
+        $this->description = $desc;
+        $this->categorie = $cat;
+        $this->pays = $pays;
+        $this->fournisseur = $fournisseur;
+        $this->lien = $lien;
+        $this->dateLimite = $date;
+    }
+
+    public function getIdBourse() { return $this->idBourse; }
+    public function setIdBourse($id) { $this->idBourse = $id; return $this; }
+
+    public function getNom() { return $this->nom; }
+    public function setNom($nom) { $this->nom = $nom; return $this; }
+
+    public function getDescription() { return $this->description; }
+    public function setDescription($desc) { $this->description = $desc; return $this; }
+
+    public function getCategorie() { return $this->categorie; }
+    public function setCategorie($cat) { $this->categorie = $cat; return $this; }
+
+    public function getPays() { return $this->pays; }
+    public function setPays($pays) { $this->pays = $pays; return $this; }
+
+    public function getFournisseur() { return $this->fournisseur; }
+    public function setFournisseur($fournisseur) { $this->fournisseur = $fournisseur; return $this; }
+
+    public function getLien() { return $this->lien; }
+    public function setLien($lien) { $this->lien = $lien; return $this; }
+
+    public function getDateLimite() { return $this->dateLimite; }
+    public function setDateLimite($date) { $this->dateLimite = $date; return $this; }
+}*/
+?>
