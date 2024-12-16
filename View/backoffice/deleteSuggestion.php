a59ada4eb4493b9ed2c7b8af87c59f18f340ee71
malek srairi <maleksrairi10@gmail.com>
2024-12-17 00:08:57 +0100

    new

--- View/backoffice/deleteSuggestion.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../../controller/suggestionC.php';
-
-$suggestionC = new SuggestionC();
-$suggestionC->deleteSuggestion($_GET["id"]);
-header('location: listSuggestions.php');
-exit();
-?>
--- model/suggestion.php
+++ model/suggestion.php
@@ -5,18 +5,20 @@ class Suggestion {
     private $date_soumission;
     private $statut;
     private $type_feedback;
-    private $id_utilisateur;
+
+    private $mail;       // Nouvel attribut mail
     private $likes;      // Nouvel attribut likes
     private $dislikes;   // Nouvel attribut dislikes
 
-    // Constructeur avec les nouveaux attributs
-    public function __construct($id_suggestion, $contenu, $date_soumission, $statut, $type_feedback, $id_utilisateur, $likes = 0, $dislikes = 0) {
+    // Constructeur avec le nouvel attribut mail
+    public function __construct($id_suggestion, $contenu, $date_soumission, $statut, $type_feedback, $mail, $likes = 0, $dislikes = 0) {
         $this->id_suggestion = $id_suggestion;
         $this->contenu = $contenu;
         $this->date_soumission = $date_soumission;
         $this->statut = $statut;
         $this->type_feedback = $type_feedback;
-        $this->id_utilisateur = $id_utilisateur;
+        
+        $this->mail = $mail;
         $this->likes = $likes;
         $this->dislikes = $dislikes;
     }
@@ -28,6 +30,7 @@ class Suggestion {
     public function getStatut() { return $this->statut; }
     public function getTypeFeedback() { return $this->type_feedback; }
     public function getIdUtilisateur() { return $this->id_utilisateur; }
+    public function getMail() { return $this->mail; }  // Getter pour mail
     public function getLikes() { return $this->likes; }  // Getter pour likes
     public function getDislikes() { return $this->dislikes; }  // Getter pour dislikes
 
@@ -38,6 +41,7 @@ class Suggestion {
     public function setStatut($statut) { $this->statut = $statut; }
     public function setTypeFeedback($type) { $this->type_feedback = $type; }
     public function setIdUtilisateur($id) { $this->id_utilisateur = $id; }
+    public function setMail($mail) { $this->mail = $mail; }  // Setter pour mail
     public function setLikes($likes) { $this->likes = $likes; }  // Setter pour likes
     public function setDislikes($dislikes) { $this->dislikes = $dislikes; }  // Setter pour dislikes
 }
